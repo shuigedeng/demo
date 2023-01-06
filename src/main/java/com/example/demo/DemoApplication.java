9fe8d6d7fc9b001d02097cdbd8e9dd80459f2bd8
shuigedeng <n3nbhbn3ymb>
2023-01-06 11:19:25 +0800

    test6

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.apache.commons.logging.LogFactoryService;
 
 @SpringBootApplication
 public class DemoApplication {
